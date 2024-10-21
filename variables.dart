5d3ba046f7a0dc71560333a0dfa90a7e8e4e6cd7
samuelgamarra <samuelgamarra169@gmail.com>
2024-10-21 14:30:15 -0500

    Create variables.dart

---
+++ variables.dart
@@ -0,0 +1,9 @@
+void main (){
+  String nombre = "samuel";
+  int edad = 16;
+
+  double precio = 1.99;
+  bool masculino = true; 
+  print ("mi nombre es $nombre, mi edad es $edad, mi precio es $precio, masculino: $masculino");
+
+}
